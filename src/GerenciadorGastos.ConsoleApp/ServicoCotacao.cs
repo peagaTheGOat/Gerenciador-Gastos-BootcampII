28d20b87a045c687e856a26969b628d892e93207
Pedro Henrique de Mello Sabino <pedrohenrique.mello.sabino@gmail.com>
2026-05-17 14:39:48 -0300

    Adiciona cotação

--- src/GerenciadorGastos.ConsoleApp/ServicoCotacao.cs
+++ src/GerenciadorGastos.ConsoleApp/ServicoCotacao.cs
@@ -12,22 +12,17 @@ namespace GerenciadorGastos.ConsoleApp
         {
             try
             {
-                // Faz a requisição na API Pública
                 var response = await _httpClient.GetStringAsync("https://economia.awesomeapi.com.br/last/USD-BRL");
                 
-                // Lê o JSON retornado
                 using var doc = JsonDocument.Parse(response);
                 var root = doc.RootElement;
-                
-                // Pega o valor específico do Dólar
+
                 var valorString = root.GetProperty("USDBRL").GetProperty("bid").GetString();
                 
-                // Converte para decimal
                 return decimal.Parse(valorString ?? "0", System.Globalization.CultureInfo.InvariantCulture);
             }
             catch
             {
-                // Se der erro de internet, retorna 0 para não quebrar o app
                 return 0m; 
             }
         }
